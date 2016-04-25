71e13b0922577c00302a8e7644858563d0353e32
Jeffrey E. Bedard <jefbed@gmail.com>
2016-04-25 03:20:37 -0400

    Convert to enum.

--- keys.h
+++ keys.h
@@ -6,39 +6,19 @@
 #ifndef __KEYS_H
 #define __KEYS_H
 
+#include <X11/keysymdef.h>
+#include <X11/Xlib.h>
+
 void jbwm_handle_key_event(XKeyEvent * restrict e);
 
-#define KEY_NEXT        XK_Tab
-#ifndef KEY_NEW
-#define KEY_NEW         XK_Return
-#endif /* !KEY_NEW */
-#define KEY_QUIT        XK_Escape
-#define KEY_TOPLEFT     XK_KP_Home
-#define KEY_TOPRIGHT    XK_KP_Page_Up
-#define KEY_BOTTOMLEFT  XK_KP_End
-#define KEY_BOTTOMRIGHT XK_KP_Page_Down
-
-#define KEY_RAISE       XK_Up
-#define KEY_LOWER       XK_Down
-#define KEY_PREVDESK    XK_Left
-#define KEY_NEXTDESK    XK_Right
-
-#define KEY_UP          XK_k
-#define KEY_DOWN        XK_j
-#define KEY_LEFT        XK_h
-#define KEY_RIGHT       XK_l
-
-#define KEY_MAX_H	XK_x
-#define KEY_MAX_V	XK_z
-
-#define KEY_ALTLOWER    XK_minus
-#define KEY_INFO        XK_F5
-#define KEY_MAX         XK_space
-#define KEY_STICK       XK_period
-#define KEY_MOVE        XK_m
-#define KEY_SHADE       XK_s
-#define KEY_FS		XK_a
-
+enum { KEY_NEXT=XK_Tab, KEY_NEW=XK_Return, KEY_QUIT=XK_Escape,
+	KEY_TOPLEFT=XK_KP_Home, KEY_TOPRIGHT=XK_KP_Page_Up,
+	KEY_BOTTOMLEFT=XK_KP_End, KEY_BOTTOMRIGHT=XK_KP_Page_Down,
+	KEY_RAISE=XK_Up, KEY_LOWER=XK_Down, KEY_PREVDESK=XK_Left,
+	KEY_NEXTDESK=XK_Right, KEY_UP=XK_k, KEY_DOWN=XK_j, KEY_LEFT=XK_h,
+	KEY_RIGHT=XK_l, KEY_MAX_H=XK_x, KEY_MAX_V=XK_z, KEY_ALTLOWER=XK_minus,
+	KEY_INFO=XK_F5, KEY_MAX=XK_space, KEY_STICK=XK_period, KEY_MOVE=XK_m,
+	KEY_SHADE=XK_s, KEY_FS=XK_a };
 /*
  * Mixtures of Ctrl, Alt an Escape are used for things like VMWare and
  * XFree86/Cygwin, so the KILL key is an option in the Makefile
@@ -47,20 +27,20 @@ void jbwm_handle_key_event(XKeyEvent * restrict e);
 #define KEY_KILL        XK_q
 #endif
 
-#define JBWM_NUM_KEYS XK_0, XK_1, XK_2, XK_3, XK_4, XK_5, XK_6, XK_7,\
-	XK_8, XK_9
+#define jbwm_num_keys xk_0, xk_1, xk_2, xk_3, xk_4, xk_5, xk_6, xk_7,\
+	xk_8, xk_9
 
-#define JBWM_VWM_KEYS KEY_PREVDESK, KEY_NEXTDESK, JBWM_NUM_KEYS
+#define jbwm_vwm_keys key_prevdesk, key_nextdesk, jbwm_num_keys
 
-#define JBWM_DIRECTION_KEYS KEY_LEFT, KEY_RIGHT, KEY_DOWN, KEY_UP
+#define jbwm_direction_keys key_left, key_right, key_down, key_up
 
-#define JBWM_KEYS_TO_GRAB KEY_NEXT, KEY_NEW, KEY_QUIT, KEY_KILL,\
-	JBWM_DIRECTION_KEYS, KEY_LOWER, KEY_ALTLOWER, KEY_MAX, KEY_STICK,\
-	JBWM_VWM_KEYS, KEY_MOVE, KEY_RAISE, KEY_SHADE, KEY_MAX_H,\
-	KEY_MAX_V, KEY_FS, 0
+#define jbwm_keys_to_grab key_next, key_new, key_quit, key_kill,\
+	jbwm_direction_keys, key_lower, key_altlower, key_max, key_stick,\
+	jbwm_vwm_keys, key_move, key_raise, key_shade, key_max_h,\
+	key_max_v, key_fs, 0
 
-#define JBWM_ALT_KEYS_TO_GRAB KEY_KILL, JBWM_DIRECTION_KEYS,\
-	JBWM_VWM_KEYS, 0
+#define jbwm_alt_keys_to_grab key_kill, jbwm_direction_keys,\
+	jbwm_vwm_keys, 0
 
 void grab_keys_for_screen(ScreenInfo * restrict s);
 
