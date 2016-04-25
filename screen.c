5dab6623bbd6c2161495f96f6d75726b974d821c
Jeffrey E. Bedard <jefbed@gmail.com>
2016-04-25 01:30:15 -0400

    Add all relevant header files.  Convert MouseMask to enum.

--- screen.c
+++ screen.c
@@ -4,19 +4,24 @@
 // See README for license and other details.
 
 #include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
 #include "atoms.h"
+#include "client_t.h"
 #include "client.h"
-#include "config.h"
 #include "ewmh.h"
-#include "jbwm.h"
+#include "jbwmenv.h"
 #include "log.h"
-#include "titlebar.h"
 #include "screen.h"
+#include "ScreenInfo.h"
 #include "shape.h"
 #include "snap.h"
+#include "titlebar.h"
 
-#define MouseMask (ButtonPressMask|ButtonReleaseMask|PointerMotionMask)
+enum {MouseMask=(ButtonPressMask|ButtonReleaseMask|PointerMotionMask)};
 
 __attribute__ ((hot))
 static void draw_outline(Client * c)
