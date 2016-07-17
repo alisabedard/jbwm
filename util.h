de69f03ba7cf2ac0227e5611feb8c0341873beb1
Jeffrey E. Bedard <jefbed@gmail.com>
2016-07-17 01:26:47 -0400

    Remove jb.h.

--- util.h
+++ util.h
@@ -4,6 +4,13 @@
 
 #include <stdbool.h>
 
+#undef MAX
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#undef MIN
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#undef SWAP
+#define SWAP(type, a, b) { type x; x = a; a = b; b = x;
+
 typedef int fd_t;
 
 // Close the FD, print an error message if unsuccessful
