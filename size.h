6ce4ca3caeed2a67678dd597132d0fe9fbc0970f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-08-24 03:28:42 -0400

    Renamed JBSize structs per size.  Added more fields.

--- size.h
+++ size.h
@@ -5,25 +5,32 @@
 
 #include <stdint.h>
 
-struct JBSize {
+struct JBSize16 {
 	union {
 		uint16_t width, w, columns, cols, col, c;
 		int16_t top, t;
+		int16_t max;
 	};
 	union {
 		uint16_t height, h, rows, r;
 		int16_t bottom, bot, b;
+		int16_t min;
 	};
 };
 
-struct JBCharSize {
+struct JBSize8 {
 	union {
 		uint8_t width, w;
+		int8_t ascent, a;
+		int8_t top, t;
+		int8_t max;
 	};
 	union {
 		uint8_t height, h;
+		int8_t descent, d;
+		int8_t bottom, bot, b;
+		int8_t min;
 	};
 };
-
 
 #endif//!JB_STRUCT_H
