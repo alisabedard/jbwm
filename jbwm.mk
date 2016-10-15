b1ad1f2069430069e6fc261f74092cad9149bfd4
Jeffrey E. Bedard <jefbed@gmail.com>
2016-10-15 16:32:04 -0400

    Add jobs flag to debug target.  Bump version to 1.50.

--- jbwm.mk
+++ jbwm.mk
@@ -3,7 +3,7 @@
 # Copyright 1999-2015, Ciaran Anscomb <jbwm@6809.org.uk>
 # See README for license and other details.
 
-version=1.49
+version=1.50
 
 PROG=jbwm
 distname=$(PROG)-$(version)
@@ -95,7 +95,7 @@ indent:
 
 debug:
 	make clean
-	make -f Makefile.debug
+	make -f Makefile.debug -j 4
 
 small:
 	make clean
