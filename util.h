24fce5561ddd18b2f9dec377fbc2ca1450010a5b
Jeffrey E. Bedard <jefbed@gmail.com>
2016-10-18 02:26:46 -0400

    Fix macro references.

--- util.h
+++ util.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #define JB_MAX(a,b) ((a) > (b) ? (a) : (b))
 #define JB_MIN(a,b) ((a) < (b) ? (a) : (b))
-#define JB_LIMIT(var, max, min) {var = MAX(MIN(var, max), min);}
+#define JB_LIMIT(var, max, min) {var = JB_MAX(JB_MIN(var, max), min);}
 #define JB_SWAP(type, a, b) {type c = a; a = b; b = c;}
 #define JB_LIKELY(x)       __builtin_expect((x), true)
 #define JB_UNLIKELY(x)     __builtin_expect((x), false)
