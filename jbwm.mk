f85ad0a9d16bc9aad8881307a413f8eb1f176286
Jeffrey E. Bedard <jefbed@gmail.com>
2016-10-23 19:22:55 -0400

    Version 1.50.

--- jbwm.mk
+++ jbwm.mk
@@ -2,7 +2,7 @@
 # Copyright 2008-2016, Jeffrey E. Bedard <jefbed@gmail.com> 
 # Copyright 1999-2015, Ciaran Anscomb <jbwm@6809.org.uk>
 # See README for license and other details.
-version=1.49
+version=1.50
 PROG=jbwm
 distname=$(PROG)-$(version)
 # Edit/override this line if you don't want jbwm to install under /usr.
