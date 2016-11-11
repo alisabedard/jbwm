a153aa5a02a1cd5457c0f6692c3396b03162f31f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-11 04:43:14 -0500

    Change include guard names.

--- class.h
+++ class.h
@@ -1,5 +1,5 @@
 // Copyright 2016, Jeffrey E. Bedard
-#ifndef JB_CLASS_H
-#define JB_CLASS_H
+#ifndef LIBJB_CLASS_H
+#define LIBJB_CLASS_H
 #define $(obj, method, ...) (obj)->method((obj), ##__VA_ARGS__)
-#endif//!JB_CLASS_H
+#endif//!LIBJB_CLASS_H
