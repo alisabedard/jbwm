a0bf6399b9b73a9b64505640ed3cccb08404a36f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-11 04:49:44 -0500

    Reindented.

--- xcb.c
+++ xcb.c
@@ -17,7 +17,7 @@ bool jb_xcb_cookie_has_error(xcb_connection_t * x, const xcb_void_cookie_t c)
 }
 static void xerr(xcb_connection_t * x, const char * msg)
 {
-        xcb_disconnect(x);
+	xcb_disconnect(x);
 	if (errno)
 		perror(msg);
 	else
@@ -26,40 +26,40 @@ static void xerr(xcb_connection_t * x, const char * msg)
 }
 void jb_check_x(xcb_connection_t * x)
 {
-       switch(xcb_connection_has_error(x)) {
-        case 0: // Success
-                break;
-        case XCB_CONN_ERROR:
-                xerr(x, "X transport error, DISPLAY unavailable");
-        case XCB_CONN_CLOSED_EXT_NOTSUPPORTED:
-                xerr(x, "Extension not supported");
-        case XCB_CONN_CLOSED_MEM_INSUFFICIENT:
-                xerr(x, "Insufficient memory");
-        case XCB_CONN_CLOSED_REQ_LEN_EXCEED:
-                xerr(x, "Request length exceeded");
-        case XCB_CONN_CLOSED_PARSE_ERR:
-                xerr(x, "Invalid DISPLAY string");
-        case XCB_CONN_CLOSED_INVALID_SCREEN:
-                xerr(x, "Invalid screen");
-        }
+	switch(xcb_connection_has_error(x)) {
+	case 0: // Success
+		break;
+	case XCB_CONN_ERROR:
+		xerr(x, "X transport error, DISPLAY unavailable");
+	case XCB_CONN_CLOSED_EXT_NOTSUPPORTED:
+		xerr(x, "Extension not supported");
+	case XCB_CONN_CLOSED_MEM_INSUFFICIENT:
+		xerr(x, "Insufficient memory");
+	case XCB_CONN_CLOSED_REQ_LEN_EXCEED:
+		xerr(x, "Request length exceeded");
+	case XCB_CONN_CLOSED_PARSE_ERR:
+		xerr(x, "Invalid DISPLAY string");
+	case XCB_CONN_CLOSED_INVALID_SCREEN:
+		xerr(x, "Invalid screen");
+	}
 }
 xcb_connection_t * jb_get_xcb_connection(const char * display, int * screen)
 {
-        xcb_connection_t * x = xcb_connect(display, screen);
+	xcb_connection_t * x = xcb_connect(display, screen);
 	jb_check_x(x);
 	return x;
 }
 xcb_screen_t * jb_get_xcb_screen(xcb_connection_t * x)
 {
-        return xcb_setup_roots_iterator(xcb_get_setup(x)).data;
+	return xcb_setup_roots_iterator(xcb_get_setup(x)).data;
 }
 pixel_t jb_get_pixel(xcb_connection_t * x, const xcb_colormap_t cmap,
 	const char * color)
 {
-	xcb_alloc_named_color_cookie_t c
-	= xcb_alloc_named_color(x, cmap, strlen(color), color);
+	xcb_alloc_named_color_cookie_t c = xcb_alloc_named_color(x,
+		cmap, strlen(color), color);
 	xcb_alloc_named_color_reply_t * r
-	= xcb_alloc_named_color_reply(x, c, NULL);
+		= xcb_alloc_named_color_reply(x, c, NULL);
 	if (jb_check(r, "Could not allocate color"))
 		return 0;
 	pixel_t p = r->pixel;
