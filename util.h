160d4b9f84edcd2bffa06270a4d22e9301756c9e
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-15 00:43:08 -0500

    Removed brackets from JB_LIMIT to allow use in if block alone--brackets were preventing else condition.  Renamed include guards.

--- util.h
+++ util.h
@@ -1,10 +1,10 @@
 // Copyright 2016, Jeffrey E. Bedard
-#ifndef JB_UTIL_H
-#define JB_UTIL_H
+#ifndef LIBJB_UTIL_H
+#define LIBJB_UTIL_H
 #include <stdbool.h>
 #define JB_MAX(a,b) ((a) > (b) ? (a) : (b))
 #define JB_MIN(a,b) ((a) < (b) ? (a) : (b))
-#define JB_LIMIT(var, max, min) {var = JB_MAX(JB_MIN(var, max), min);}
+#define JB_LIMIT(var, max, min) var = JB_MAX(JB_MIN(var, max), min);
 #define JB_SWAP(type, a, b) {type c = a; a = b; b = c;}
 #define JB_LIKELY(x)       __builtin_expect((x), true)
 #define JB_UNLIKELY(x)     __builtin_expect((x), false)
@@ -15,4 +15,4 @@ bool jb_check(const bool val, const char * msg);
 void jb_require(const bool val, const char * msg);
 // If val is false, print msg then abort()
 void jb_assert(const bool val, char * msg);
-#endif//!JB_UTIL_H
+#endif//!LIBJB_UTIL_H
