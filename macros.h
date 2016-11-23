146322cd470c1859fa96ccf81c3fbc585a3db028
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-23 02:40:26 -0500

    Added macros.h from libjb.

---
+++ macros.h
@@ -0,0 +1,10 @@
+// Copyright 2016, Jeffrey E. Bedard
+#ifndef LIBJB_MACROS_H
+#define LIBJB_MACROS_H
+#define JB_MAX(a,b) ((a) > (b) ? (a) : (b))
+#define JB_MIN(a,b) ((a) < (b) ? (a) : (b))
+#define JB_LIMIT(var, max, min) var = JB_MAX(JB_MIN(var, max), min);
+#define JB_SWAP(type, a, b) {type c = a; a = b; b = c;}
+#define JB_LIKELY(x)       __builtin_expect((x), true)
+#define JB_UNLIKELY(x)     __builtin_expect((x), false)
+#endif//!LIBJB_MACROS_H
