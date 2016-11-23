3007096d7018ea283b87b7c733f83acfada72232
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-23 02:35:15 -0500

    Split macros to separate header.

--- util.h
+++ util.h
@@ -2,12 +2,6 @@
 #ifndef LIBJB_UTIL_H
 #define LIBJB_UTIL_H
 #include <stdbool.h>
-#define JB_MAX(a,b) ((a) > (b) ? (a) : (b))
-#define JB_MIN(a,b) ((a) < (b) ? (a) : (b))
-#define JB_LIMIT(var, max, min) var = JB_MAX(JB_MIN(var, max), min);
-#define JB_SWAP(type, a, b) {type c = a; a = b; b = c;}
-#define JB_LIKELY(x)       __builtin_expect((x), true)
-#define JB_UNLIKELY(x)     __builtin_expect((x), false)
 typedef int fd_t;
 // If val is false, print msg then return !val
 bool jb_check(const bool val, const char * msg);
