70d402a7d53e6b550216305fa27d6800e23cd404
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-28 06:50:53 -0500

    Split logically and added new fields.  Pull from libjb.

--- JBDim.h
+++ JBDim.h
@@ -3,13 +3,25 @@
 #define LIBJB_JBDIM_H
 #include <stdint.h>
 struct JBDim {
+	/* Note:  Everything except width and height, and their aliases
+	   w and h, should be kept as int16_t.  */
 	union {
-		uint16_t width, w, columns, cols, col, c;
-		int16_t x, top, t, max, start;
+		uint16_t width, w;
+		int16_t columns, cols, col, c;
+		int16_t maximum, max;
+		int16_t start;
+		int16_t top, t;
+		int16_t x;
 	};
 	union {
-		uint16_t height, h, row, rows, r;
-		int16_t y, bottom, bot, b, min, index, end;
+		uint16_t height, h;
+		int16_t bottom, bot, b;
+		int16_t end;
+		int16_t index;
+		int16_t length;
+		int16_t minimum, min;
+		int16_t row, rows, r;
+		int16_t y;
 	};
 };
 #endif//!LIBJB_JBDIM_H
