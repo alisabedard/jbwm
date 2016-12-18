e72c8d6fb8ed123fd55be22f397f25daa64d873b
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-18 03:16:13 -0500

    Updated.

--- depend.mk
+++ depend.mk
@@ -5,15 +5,26 @@ client.o: client.c client.h JBWMClient.h JBWMScreen.h JBDim.h pixel_t.h \
  util.h
 display.o: display.c display.h client.h config.h events.h font.h jbwm.h \
  log.h
+events.o: events.c events.h JBWMScreen.h JBDim.h pixel_t.h button_event.h \
+ client.h display.h ewmh_state.h ewmh.h jbwm.h keys.h log.h new.h \
+ screen.h select.h title_bar.h util.h wm_state.h JBWMClient.h
 ewmh.o: ewmh.c ewmh.h JBWMClient.h JBWMDefaults.h JBWMScreen.h JBDim.h \
  pixel_t.h client.h config.h display.h font.h log.h macros.h util.h
 ewmh_state.o: ewmh_state.c ewmh_state.h JBWMScreen.h JBDim.h pixel_t.h \
  client.h display.h ewmh.h jbwm.h log.h max.h screen.h select.h util.h \
  wm_state.h JBWMClient.h
 font.o: font.c font.h config.h jbwm.h
+jbwm.o: jbwm.c jbwm.h JBWMScreen.h JBDim.h pixel_t.h config.h display.h \
+ events.h ewmh.h keys.h log.h new.h util.h
+keys.o: keys.c keys.h JBWMDefaults.h JBWMScreen.h JBDim.h pixel_t.h \
+ client.h config.h display.h jbwm.h log.h max.h screen.h select.h snap.h \
+ title_bar.h util.h wm_state.h JBWMClient.h
 max.o: max.c max.h JBWMClient.h JBWMScreen.h JBDim.h pixel_t.h display.h \
  ewmh.h ewmh_state.h font.h log.h screen.h title_bar.h
 mwm.o: mwm.c mwm.h JBWMClient.h display.h log.h util.h pixel_t.h
+new.o: new.c new.h JBWMClient.h JBWMDefaults.h JBWMScreen.h JBDim.h \
+ pixel_t.h client.h config.h display.h ewmh.h jbwm.h keys.h log.h \
+ macros.h mwm.h screen.h select.h shape.h util.h
 screen.o: screen.c screen.h JBWMDefaults.h JBWMScreen.h JBDim.h pixel_t.h \
  client.h config.h display.h ewmh_state.h ewmh.h font.h jbwm.h shape.h \
  snap.h title_bar.h util.h wm_state.h JBWMClient.h
