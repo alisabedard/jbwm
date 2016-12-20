48f2379102716d2923be2bdb2a56877ac2a9355e
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-20 02:58:54 -0500

    Used XftFontOpenName to open xft fonts, allowing all font properties to be specified in one setting.  Changed default font properties to size 10 and aspect 1.2.

--- config.h
+++ config.h
@@ -8,8 +8,8 @@
 #define JBWM_DEF_TERM "xterm"
 /* Default fonts */
 #ifdef JBWM_USE_XFT
-#define JBWM_DEF_FONT "Sans"
-#define JBWM_FONT_SIZE 11.0
+//#define JBWM_DEF_FONT "Sans:size=10:aspect=1.25:weight=medium"
+#define JBWM_DEF_FONT "Sans:size=10:aspect=1.2"
 #else /* ! JBWM_USE_XFT */
 #define JBWM_DEF_FONT "variable"
 #endif /* JBWM_USE_XFT */
@@ -18,19 +18,19 @@
 #define JBWM_DEF_FC "#8f8"
 #define JBWM_DEF_BG "#111"
 #define JBWM_DEF_CLOSE "#867"
-#define JBWM_DEF_SHADE "#6a7"
-#define JBWM_DEF_STICK "#555"
 #define JBWM_DEF_RESIZE "#66a"
+#define JBWM_DEF_SHADE "#555"
+#define JBWM_DEF_STICK "#6a7"
 // Environment
+#define JBWM_ENV_BG "JBWM_BG"
 #define JBWM_ENV_DISPLAY "DISPLAY"
 #define JBWM_ENV_FG "JBWM_FG"
 #define JBWM_ENV_FC "JBWM_FC"
-#define JBWM_ENV_BG "JBWM_BG"
 #define JBWM_ENV_CLOSE "JBWM_CLOSE"
+#define JBWM_ENV_FONT "JBWM_FONT"
+#define JBWM_ENV_RESIZE "JBWM_RESIZE"
 #define JBWM_ENV_SHADE "JBWM_SHADE"
 #define JBWM_ENV_STICK "JBWM_STICK"
-#define JBWM_ENV_RESIZE "JBWM_RESIZE"
-#define JBWM_ENV_FONT "JBWM_FONT"
 #define JBWM_ENV_TERM "JBWM_TERM"
 #define JBWM_NAME "jbwm"
 #endif//JBWM_CONFIG_H
--- font.c
+++ font.c
@@ -17,9 +17,7 @@ void jbwm_open_font(Display * d)
 		return; // already created
 	char * font = getenv(JBWM_ENV_FONT);
 #ifdef JBWM_USE_XFT
-	jbwm_font = XftFontOpen(d, DefaultScreen(d), XFT_FAMILY,
-		XftTypeString, font, XFT_SIZE, XftTypeDouble,
-		JBWM_FONT_SIZE, NULL);
+	jbwm_font = XftFontOpenName(d, DefaultScreen(d), font);
 #else//!JBWM_USE_XFT
 	jbwm_font = XLoadQueryFont(d, font);
 #endif//JBWM_USE_XFT
@@ -36,7 +34,6 @@ uint8_t jbwm_get_font_ascent(void)
 }
 uint8_t jbwm_get_font_height(void)
 {
-	return jbwm_font->ascent
-		+ jbwm_font->descent;
+	return jbwm_font->ascent + jbwm_font->descent;
 }
 
