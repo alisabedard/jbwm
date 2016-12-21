6b50b3db58ad4c22661546d3724cc05838ede410
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-21 05:13:00 -0500

    Reduced display accessor calls.  Eliminated length variable.

--- title_bar.c
+++ title_bar.c
@@ -17,23 +17,23 @@
 #include "select.h"
 #include "util.h"
 #include "wm_state.h"
-static void set_shaded(struct JBWMClient * restrict c)
+static void set_shaded(Display * d, struct JBWMClient * restrict c)
 {
 	c->old_size.height = c->size.height;
 	c->size.height = -1;
 	c->opt.shaded = true;
 	jbwm_set_wm_state(c, IconicState);
-	jbwm_ewmh_add_state(jbwm_get_display(), c->window,
+	jbwm_ewmh_add_state(d, c->window,
 		jbwm_ewmh_get_atom(JBWM_EWMH_WM_STATE_SHADED));
 	jbwm_select_client(c);
 }
-static void set_not_shaded(struct JBWMClient * restrict c)
+static void set_not_shaded(Display * d, struct JBWMClient * restrict c)
 {
 	c->size.height = c->old_size.height;
 	c->opt.shaded = false;
 	jbwm_move_resize(c);
 	jbwm_set_wm_state(c, NormalState);
-	jbwm_ewmh_remove_state(jbwm_get_display(), c->window,
+	jbwm_ewmh_remove_state(d, c->window,
 		jbwm_ewmh_get_atom(JBWM_EWMH_WM_STATE_SHADED));
 }
 void jbwm_toggle_shade(struct JBWMClient * restrict c)
@@ -42,7 +42,7 @@ void jbwm_toggle_shade(struct JBWMClient * restrict c)
 	if (c->opt.no_min || c->opt.fullscreen)
 		return;
 	// This implements window shading, a substitute for iconification.
-	(c->opt.shaded ? set_not_shaded : set_shaded)(c);
+	(c->opt.shaded ? set_not_shaded : set_shaded)(jbwm_get_display(), c);
 	jbwm_update_title_bar(c);
 }
 static uint16_t mv(Display * d, const Window w, uint16_t x)
@@ -65,10 +65,9 @@ static Window get_win(Display * d, const Window p,
 	uint8_t h = jbwm_get_font_height();
 	return XCreateSimpleWindow(d, p, 0, 0, h, h, 0, 0, bg);
 }
-static Window new_title_bar(struct JBWMClient * restrict c)
+static Window new_title_bar(Display * d, struct JBWMClient * restrict c)
 {
 	const struct JBWMPixels * p = &jbwm_get_screen(c)->pixels;
-	Display * d = jbwm_get_display();
 	const Window t = c->tb.win = get_win(d, c->parent, p->bg);
 	XSelectInput(d, t, ExposureMask);
 	struct JBWMClientOptions * o = &c->opt;
@@ -82,12 +81,11 @@ static Window new_title_bar(struct JBWMClient * restrict c)
 	return t;
 }
 static void
-draw_xft(struct JBWMClient * restrict c, const int16_t * restrict p,
-	char * name, const size_t l)
+draw_xft(Display * d, struct JBWMClient * restrict c,
+	const int16_t * restrict p, char * name, const size_t l)
 {
 	XftFont * f = jbwm_get_font();
 	XGlyphInfo e;
-	Display * d = jbwm_get_display();
 	XftTextExtentsUtf8(d, f, (XftChar8 *) name, l, &e);
 	const uint8_t s = c->screen;
 	Visual *v = DefaultVisual(d, s);
@@ -112,22 +110,20 @@ static char * jbwm_get_title(Display * d, const Window w)
 		  return NULL;
 	return (char *)tp.value;
 }
-static void draw_title(struct JBWMClient * restrict c)
+static void draw_title(Display * d, struct JBWMClient * restrict c)
 {
-	Display * d = jbwm_get_display();
 	char * name = jbwm_get_title(d, c->window);
 	if(!name)
 		return; // No title could be loaded, abort
 	const int16_t p[] = {jbwm_get_font_height() + 4,
 		jbwm_get_font_ascent()};
-	const size_t l = strlen(name);
-	draw_xft(c, p, name, l);
+	draw_xft(d, c, p, name, strlen(name));
 	XFree(name);
 }
-static void remove_title_bar(struct JBWMClient * restrict c)
+static void remove_title_bar(Display * d, struct JBWMClient * restrict c)
 {
 	++(c->ignore_unmap);
-	XDestroyWindow(jbwm_get_display(), c->tb.win);
+	XDestroyWindow(d, c->tb.win);
 	c->tb.win = 0;
 }
 void jbwm_update_title_bar(struct JBWMClient * restrict c)
@@ -135,23 +131,21 @@ void jbwm_update_title_bar(struct JBWMClient * restrict c)
 	if (c->opt.shaped)
 		return;
 	Window w = c->tb.win;
+	Display * d = jbwm_get_display();
 	if (c->opt.fullscreen && w) {
-		remove_title_bar(c);
+		remove_title_bar(d, c);
 		return;
 	}
 	if (!w)
-		w = new_title_bar(c);
-	{ // * d scope
-		Display * d = jbwm_get_display();
-		/* Expand/Contract the title bar width as necessary:  */
-		{ // width scope
-			const uint16_t width = c->size.width;
-			XResizeWindow(d, w, width, jbwm_get_font_height());
-			move_buttons(d, &c->tb, width);
-		}
-		XClearWindow(d, w);
+		w = new_title_bar(d, c);
+	/* Expand/Contract the title bar width as necessary:  */
+	{ // width scope
+		const uint16_t width = c->size.width;
+		XResizeWindow(d, w, width, jbwm_get_font_height());
+		move_buttons(d, &c->tb, width);
 	}
-	draw_title(c);
+	XClearWindow(d, w);
+	draw_title(d, c);
 	if (c->opt.no_title_bar)
-		remove_title_bar(c);
+		remove_title_bar(d, c);
 }
