d4a9e36ae9800c028ac80b8fae3b68aaa70f0a84
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-30 02:04:32 -0500

    Added JB_LOG_ADD, JB_LOG_DEL, and JB_LOG_OP.

--- cpp.h
+++ cpp.h
@@ -8,5 +8,14 @@
 #define JBCERR std::cerr << __FILE__ << ':' << __LINE__ << ' '
 #define libjb_strlen(s) std::string(s).size()
 #define libjb_strlcpy(dst, src, sz) std::string(src).copy(dst, sz)
+#ifdef DEBUG
+#define JB_LOG_ADD(cls, v) JBCERR << "NEW " #cls "--> " << ++v << '\n';
+#define JB_LOG_DEL(cls, v) JBCERR << "DEL " #cls "--> " << --v << '\n';
+#define JB_LOG_OP(cls, msg) JBCERR << "@"#cls << ' ' << msg << '\n' ;
+#else//!DEBUG
+#define JB_LOG_ADD(c, d)
+#define JB_LOG_DEL(c, d)
+#define JB_LOG_OP(c, d)
+#endif//DEBUG
 #endif//!LIBJB_CPP_H
 #endif//__cplusplus
