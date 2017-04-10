09f9002325c1b7e2db13ea4fdcf8c3f6dbebe2a0
Jeffrey E. Bedard <jefbed@gmail.com>
2017-04-10 00:29:35 -0400

    Reformatted comment.

--- util.h
+++ util.h
@@ -17,8 +17,8 @@ void jb_require(const bool val, const char * msg);
 // Execute command in a new background shell
 void jb_shell(const char * command);
 /* Execute command in a new background process.  Note, this does not handle
-   quoted arguments properly.  For that, use jb_shell().  This version
-   has the advantage of not spawning a shell process.  */
+   quoted arguments properly.  For that, use jb_shell().  This version has the
+   advantage of not spawning a shell process.  */
 void jb_system(const char * command);
 #ifdef __cplusplus
 }
