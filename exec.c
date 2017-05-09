d61960a2401669a09aeb83a3f3b21de78c907308
Jeffrey E. Bedard <jefbed@gmail.com>
2017-05-09 04:33:08 -0400

    Split out jbwm_exec.  Called command with execl, in the same manner as system(), per the manual page.

--- exec.c
+++ exec.c
@@ -13,13 +13,16 @@ static void jbwm_wait_child(void)
 {
 	signal(SIGCHLD, sigchld_cb); // reap zombies
 }
-void jbwm_start_terminal(void)
+static void jbwm_exec(const char * command)
 {
 	if (fork() == 0) {
-		char * t = getenv(JBWM_ENV_TERM);
-		execlp(t, t, (char*)NULL);
+		execl("/bin/sh", "sh", "-c", command, NULL);
 		// This is only reached on error.
 		exit(1);
 	} else // in controlling process:
 		jbwm_wait_child();
+}
+void jbwm_start_terminal(void)
+{
+	jbwm_exec(getenv(JBWM_ENV_TERM));
 }
