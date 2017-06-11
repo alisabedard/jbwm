4138bb251e972d50dc64c2b8cdeed330e7f3751a
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-11 14:55:14 -0400

    Stored prefix the list then isolate later with car/cdr.  As a result, create a super-procedure print-each to call print-each-array or print-each-enum as applicable.  Restored the output port to default at the end of execution.

--- ewmh.scm
+++ ewmh.scm
@@ -1,16 +1,18 @@
 ":";exec guile -s $0 "$@"
 ; for MIT Scheme
 (define copyright "Copyright 2017, Jeffrey E. Bedard")
-(define net '("SUPPORTED" "CURRENT_DESKTOP" "NUMBER_OF_DESKTOPS"
+; Store the prefix as the first element in these lists:
+(define net '("" "SUPPORTED" "CURRENT_DESKTOP" "NUMBER_OF_DESKTOPS"
 	"DESKTOP_VIEWPORT" "DESKTOP_GEOMETRY" "SUPPORTING_WM_CHECK"
 	"ACTIVE_WINDOW" "MOVERESIZE_WINDOW" "CLOSE_WINDOW" "CLIENT_LIST"
 	"VIRTUAL_ROOTS" "CLIENT_LIST_STACKING" "FRAME_EXTENTS"))
-(define wm '("ALLOWED_ACTIONS" "NAME" "DESKTOP" "MOVERESIZE" "PID"
+(define wm '("WM_" "ALLOWED_ACTIONS" "NAME" "DESKTOP" "MOVERESIZE" "PID"
 	"WINDOW_TYPE" "STATE"))
-(define wm-action '("MOVE" "RESIZE" "CLOSE" "SHADE" "FULLSCREEN"
-	"CHANGE_DESKTOP" "ABOVE" "BELOW" "MAXIMIZE_HORZ" "MAXIMIZE_VERT"))
-(define wm-state '("STICKY" "MAXIMIZED_VERT" "MAXIMIZED_HORZ" "SHADED"
-	"HIDDEN" "FULLSCREEN" "ABOVE" "BELOW" "FOCUSED"))
+(define wm-action '("WM_ACTION_" "MOVE" "RESIZE" "CLOSE" "SHADE"
+	"FULLSCREEN" "CHANGE_DESKTOP" "ABOVE" "BELOW" "MAXIMIZE_HORZ"
+	"MAXIMIZE_VERT"))
+(define wm-state '("WM_STATE_" "STICKY" "MAXIMIZED_VERT" "MAXIMIZED_HORZ"
+	"SHADED" "HIDDEN" "FULLSCREEN" "ABOVE" "BELOW" "FOCUSED"))
 (define get-copyright-line (lambda ()
 	(string-append "// " copyright "\n")))
 (define begin-array-definition (lambda (name) (begin
@@ -21,25 +23,28 @@
 	(display (string-append "enum " name "{\n"))))
 (define end-c-definition (lambda () (display "};\n")))
 (define master-prefix "_NET_")
-(define print-each (lambda (prefix l)
+(define endl (lambda () (display "\n") 0))
+(define print-each-array (lambda (prefix l)
 	(if (null? l)
-		(begin (display "\n") 0)
+		(endl)
 		(begin
-			(display (string-append "\t\"" master-prefix
-				prefix (car l) "\",\n"))
-			(print-each prefix (cdr l))))))
+			(display (string-append "\t\"" master-prefix prefix
+				(car l) "\",\n"))
+			(print-each-array prefix (cdr l))))))
 (define get-enum-line (lambda (prefix item)
 	(string-append "\t" master-prefix prefix item ",\n")))
 (define print-enum-line (lambda (prefix item)
 	(display (get-enum-line prefix item))))
 (define print-each-enum (lambda (prefix l)
 	(if (null? l)
-		(begin (display "\n") 0)
+		(endl)
 		(begin
 			(print-enum-line prefix (car l))
 			(print-each-enum prefix (cdr l))))))
 (define get-guard (lambda (name) 
 	(string-append "JBWM_" name "_H\n")))
+(define print-each (lambda (function data)
+	(function (car data) (cdr data))))
 
 ; The execution begins:
 
@@ -47,12 +52,11 @@
 (define f (open-output-file "ewmh_atoms.c"))
 (set-current-output-port! f)
 (begin-array-definition "jbwm_atom_names")
-(print-each "" net)
-(print-each "WM_" wm)
-(print-each "WM_ACTION_" wm-action)
-(print-each "WM_STATE_" wm-state)
+(print-each print-each-array net)
+(print-each print-each-array wm)
+(print-each print-each-array wm-action)
+(print-each print-each-array wm-state)
 (end-c-definition)
-;(display "};\n")
 (close-port f)
 
 ; JBWMAtomIndex:
@@ -65,12 +69,14 @@
 	"#define " (get-guard ig)))
 (begin-enum-definition "JBWMAtomIndex")
 (set! master-prefix "JBWM_EWMH_")
-(print-each-enum "" net)
-(print-each-enum "WM_" wm)
-(print-each-enum "WM_ACTION_" wm-action)
-(print-each-enum "WM_STATE_" wm-state)
+(print-each print-each-enum net)
+(print-each print-each-enum wm)
+(print-each print-each-enum wm-action)
+(print-each print-each-enum wm-state)
 (display "\t// The following entry must be last:\n")
 (print-enum-line "" "ATOMS_COUNT")
 (end-c-definition)
 (display (string-append "#endif//!" (get-guard ig)))
 (close-port f)
+; Restore normal output:
+(set-current-output-port! console-i/o-port)
