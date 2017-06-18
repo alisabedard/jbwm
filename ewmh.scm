007b2106f86252afa760ed7da2003a31dd324849
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-18 01:02:51 -0400

    Split out reusable functions into separate file.

--- ewmh.scm
+++ ewmh.scm
@@ -1,5 +1,6 @@
-; for MIT Scheme
 (define copyright "// Copyright 2017, Jeffrey E. Bedard\n")
+; for MIT Scheme
+(load "libconvert.scm")
 ; Store the prefix as the first element in these lists:
 (define net '("" "SUPPORTED" "CURRENT_DESKTOP" "NUMBER_OF_DESKTOPS"
 	"DESKTOP_VIEWPORT" "DESKTOP_GEOMETRY" "SUPPORTING_WM_CHECK"
@@ -12,28 +13,6 @@
 	"MAXIMIZE_VERT"))
 (define wm-state '("WM_STATE_" "STICKY" "MAXIMIZED_VERT" "MAXIMIZED_HORZ"
 	"SHADED" "HIDDEN" "FULLSCREEN" "ABOVE" "BELOW" "FOCUSED"))
-; The following prefix is applied to each entry
-(define master-prefix "_NET_")
-(define begin-array-definition (lambda (name)
-	(display (string-append copyright
-		"static char * " name " [] = {\n"))))
-(define begin-enum-definition (lambda (name)
-	(display (string-append "enum " name " {\n"))))
-(define end-c-definition (lambda () (display "};\n")))
-(define get-array-line (lambda (prefix item)
-	(string-append "\t\"" master-prefix prefix item "\",\n")))
-(define print-each-array-element (lambda (prefix l)
-	(map (lambda (item) (display (get-array-line prefix item))) l)))
-(define get-enum-line (lambda (prefix item)
-	(string-append "\t" master-prefix prefix item ",\n")))
-(define print-enum-line (lambda (prefix item)
-	(display (get-enum-line prefix item))))
-(define print-each-enum (lambda (prefix l)
-	(map (lambda (item) (print-enum-line prefix item)) l)))
-(define get-guard (lambda (name) 
-	(string-append "JBWM_" name "_H\n")))
-(define print-each (lambda (function data)
-	(function (car data) (cdr data))))
 (define print-all (lambda (function)
 	(map (lambda (cat) (print-each function cat))
 		(list net wm wm-action wm-state))))
@@ -45,12 +24,6 @@
 	(print-all print-each-array-element)
 	(end-c-definition)
 	(close-port outf))))
-(define write-include-header (lambda (ig)
-	(display (string-append copyright
-		"#ifndef " (get-guard ig)
-		"#define " (get-guard ig)))))
-(define write-include-fin (lambda (ig) (display (string-append "#endif//!"
-	(get-guard ig)))))
 ; Generate "JBWMAtomIndex.h"
 (define write-jbwmatomindex-h (lambda ()
 	(let ((outf (open-output-file "JBWMAtomIndex.h"))
