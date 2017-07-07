14f3e89336632c767e4c5a608e1fda3948e4d52e
Jeffrey E. Bedard <jefbed@gmail.com>
2017-07-07 00:42:00 -0400

    Generated JBWMKeys from keys.txt using keys.scm.  Reformatted libconvert.scm. Added display-c-include.

---
+++ keys.scm
@@ -0,0 +1,27 @@
+; Copyright 2017, Jeffrey E. Bedard
+; vim: sw=2
+(load "libconvert.scm")
+(define convert_keys
+ (lambda (in_filename out_filename)
+  (letrec ((i (open-input-file in_filename))
+	   (o (open-output-file out_filename))
+	   (guard "JBWM_JBWMKEYS")
+	   (parse (lambda (i o)
+		   (let ((line (read-line i)))
+		    (if (and (not (eof-object? line))
+			 (> (string-length line) 1))
+		     (let ((key (string-car line))
+			   (value (string-cdr line)))
+		      (display (string-append "\tJBWM_KEY_" key
+				" = XK_" value ",\n") o)
+		      (parse i o)))))))
+   (begin-include guard o)
+   (display-c-include "<X11/keysym.h>" o)
+   (display "enum JBWMKeys {\n" o)
+   (parse i o)
+   (display "};\n" o)
+   (display-c-include "\"key_combos.h\"" o)
+   (end-include guard o)
+   (close-port i)
+   (close-port o))))
+(convert_keys "keys.txt" "JBWMKeys.h")
