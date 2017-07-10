1c247fc2b58fa8ed853ac17ad730512fc860c336
Jeffrey E. Bedard <jefbed@gmail.com>
2017-07-09 23:16:07 -0400

    Used straight-forward implementation of all possible mwm hints.  Unified point of return in jbwm_handle_mwm_hints().

--- mwm.c
+++ mwm.c
@@ -7,67 +7,33 @@
 #include "log.h"
 #include "mwmproto.h"
 #include "util.h"
-//#define JBWM_ENABLE_NO_RESIZE_HINT
-#define JBWM_ENABLE_TITLE_BAR_HINT
 static void process_flags(struct JBWMClient * c)
 {
 	struct JBWMClientOptions * o = &c->opt;
 	if (o->tearoff) {
-		o->no_resize = o->no_shade = o->no_max = o->no_title_bar = true;
+		o->no_resize = o->no_shade = o->no_max
+			= o->no_title_bar = true;
 		o->border = 0;
 	}
 }
-static void do_functions(struct JBWMClientOptions * o,
+static void do_functions(struct JBWMClientOptions * restrict o,
 	const enum MwmFunctions f)
 {
-	/* Qt 4 and 5 exhibit bad behavior if these three checks are
-	   not disabled.  Since Qt 4 and 5 applications are much more
-	   common than Motif applications today, defer to behavior
-	   which works with both.  */
-#ifdef JBWM_NO_QT_FIX
-#ifdef JBWM_ENABLE_NO_RESIZE_HINT
-	if (!(f & MWM_FUNC_RESIZE))
-		o->no_resize = true;
-#endif//JBWM_ENABLE_NO_RESIZE_HINT
-	if (!(f & MWM_FUNC_MINIMIZE))
-		o->no_shade = true;
-	if (!(f & MWM_FUNC_CLOSE))
-		o->no_close = true;
-#endif//JBWM_NO_QT_FIX
-#ifdef ENABLE_NO_MAX
-	if (!(f & MWM_FUNC_MAXIMIZE))
-		o->no_max = true;
-#endif//ENABLE_NO_MAX
-	if (!(f & MWM_FUNC_MOVE))
-		o->no_move = true;
-	JBWM_LOG("MWM_HINTS_FUNCTIONS\topts: %d, %d, %d, %d, %d",
-		o->no_resize, o->no_close, o->no_shade, o->no_max,
-		o->no_move);
+	o->no_close = !(f & MWM_FUNC_CLOSE);
+	o->no_max = !(f & MWM_FUNC_MAXIMIZE);
+	o->no_move = !(f & MWM_FUNC_MOVE);
+	o->no_resize = !(f & MWM_FUNC_RESIZE);
+	o->no_shade = !(f & MWM_FUNC_MINIMIZE);
 }
-static void do_decorations(struct JBWMClientOptions * o,
+static void do_decorations(struct JBWMClientOptions * restrict o,
 	const enum MwmDecor f)
 {
 	o->border = f & MWM_DECOR_BORDER ? 1 : 0;
-#ifdef ENABLE_NO_RESIZE
-	if (f & MWM_DECOR_RESIZEH) {
-		JBWM_LOG("decor resize");
-		o->no_resize = false;
-	}
-#endif//ENABLE_NO_RESIZE
-	if (f & MWM_DECOR_MENU) {
-		JBWM_LOG("decor close");
-		o->no_close = false;
-	}
+	o->no_close = !(f & MWM_DECOR_MENU);
+	o->no_max = !(f & MWM_DECOR_MAXIMIZE);
+	o->no_resize = !(f & MWM_DECOR_RESIZEH);
 	o->no_shade = !(f & MWM_DECOR_MINIMIZE);
-#ifdef JBWM_ENABLE_TITLE_BAR_HINT
-	if (f & MWM_DECOR_TITLE) {
-		JBWM_LOG("decor title_bar");
-		o->no_title_bar = false;
-	} else {
-		JBWM_LOG("decor no_title_bar");
-		o->no_title_bar = true;
-	}
-#endif//JBWM_ENABLE_TITLE_BAR_HINT
+	o->no_title_bar = !(f & MWM_DECOR_TITLE);
 }
 static Atom get_mwm_hints_atom(Display * d)
 {
@@ -80,18 +46,19 @@ void jbwm_handle_mwm_hints(struct JBWMClient * c)
 	const Atom mwm_hints = get_mwm_hints_atom(d);
 	struct JBWMMwm * m = jbwm_get_property(d, c->window,
 		mwm_hints, &(uint16_t){0});
-	if(!m)
-		return;
-	if (!((c->opt.tearoff = m->flags & MWM_HINTS_STATUS && m->status &
-		MWM_TEAROFF_WINDOW))) {
-		// skip the following if tear-off window
-		if (m->flags & MWM_HINTS_FUNCTIONS
-			&& !(m->functions & MWM_FUNC_ALL))
-			do_functions(&c->opt, m->functions);
-		if (m->flags & MWM_HINTS_DECORATIONS
-			&& !(m->decor & MWM_DECOR_ALL))
-			do_decorations(&c->opt, m->decor);
+	if (m) { // property successfully retrieved
+		if (!((c->opt.tearoff = m->flags
+			& MWM_HINTS_STATUS && m->status &
+			MWM_TEAROFF_WINDOW))) {
+			// skip the following if tear-off window
+			if (m->flags & MWM_HINTS_FUNCTIONS
+				&& !(m->functions & MWM_FUNC_ALL))
+				do_functions(&c->opt, m->functions);
+			if (m->flags & MWM_HINTS_DECORATIONS
+				&& !(m->decor & MWM_DECOR_ALL))
+				do_decorations(&c->opt, m->decor);
+		}
+		XFree(m);
+		process_flags(c);
 	}
-	XFree(m);
-	process_flags(c);
 }
