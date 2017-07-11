c6f93bae696502670d13479eff80f5848ff9cb06
Jeffrey E. Bedard <jefbed@gmail.com>
2017-07-10 23:54:59 -0400

    Used and-let* special form in parse.

--- keys.scm
+++ keys.scm
@@ -3,18 +3,19 @@
 (load "libconvert.scm")
 (define convert_keys
  (lambda (in_filename out_filename)
-  (letrec ((i (open-input-file in_filename))
-	   (o (open-output-file out_filename))
-	   (guard "JBWM_JBWMKEYS")
-	   (parse (lambda (i o)
-		   (let ((line (read-line i)))
-		    (if (and (not (eof-object? line))
-			 (> (string-length line) 1))
-		     (let ((key (string-car line))
-			   (value (string-cdr line)))
-		      (display (string-append "\tJBWM_KEY_" key
-				" = XK_" value ",\n") o)
-		      (parse i o)))))))
+  (letrec
+   ((i (open-input-file in_filename))
+    (o (open-output-file out_filename))
+    (guard "JBWM_JBWMKEYS")
+    (parse
+     (lambda (i o)
+      (and-let*
+       ((line (read-line i))
+	((not (eof-object? line)))
+	((> (string-length line) 1)))
+       (let ((key (string-car line)) (value (string-cdr line)))
+	(display (string-append "\tJBWM_KEY_" key " = XK_" value ",\n") o)
+	(parse i o))))))
    (begin-include guard o)
    (display-c-include "<X11/keysym.h>" o)
    (display "enum JBWMKeys {\n" o)
