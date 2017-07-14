24ba9f9bfd0f6c9d1ef97d5271615391da86ae3d
Jeffrey E. Bedard <jefbed@gmail.com>
2017-07-14 01:15:25 -0400

    Consolidated point of return in jbwm_set_vdesk().

--- vdesk.c
+++ vdesk.c
@@ -10,7 +10,6 @@
 #include "client.h"
 #include "config.h"
 #include "ewmh.h"
-#include "log.h"
 #include "util.h"
 static void check_visibility(const struct JBWMScreen * s, struct
 	JBWMClient * restrict c, const uint8_t v)
@@ -29,15 +28,14 @@ static void check_visibility(const struct JBWMScreen * s, struct
 }
 uint8_t jbwm_set_vdesk(Display * d, struct JBWMScreen * s, uint8_t v)
 {
-	JBWM_LOG("jbwm_set_vdesk");
-	if (v == s->vdesk || v > JBWM_MAX_DESKTOPS)
-		return s->vdesk;
-	check_visibility(s, jbwm_get_head_client(), v);
-	s->vdesk = v;
-	/* Send anonymous variable of type uint32_t in order
-	   to avoid potentially invalid data.  */
-	jbwm_set_property(d, RootWindowOfScreen(s->xlib),
-		jbwm_ewmh_get_atom(JBWM_EWMH_CURRENT_DESKTOP),
-		XA_CARDINAL, &(uint32_t){v}, 1);
+	if (v != s->vdesk && v <= JBWM_MAX_DESKTOPS) {
+		check_visibility(s, jbwm_get_head_client(), v);
+		s->vdesk = v;
+		/* Send anonymous variable of type uint32_t in order
+		   to avoid potentially invalid data.  */
+		jbwm_set_property(d, RootWindowOfScreen(s->xlib),
+			jbwm_ewmh_get_atom(JBWM_EWMH_CURRENT_DESKTOP),
+			XA_CARDINAL, &(uint32_t){v}, 1);
+	}
 	return s->vdesk;
 }
