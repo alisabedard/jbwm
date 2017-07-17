b2d1ee7177d0f39f9fd4878c314459cbb1784cff
Jeffrey E. Bedard <jefbed@gmail.com>
2017-07-17 01:05:19 -0400

    Removed to fix connection.

--- libjb/depend.mk
+++
@@ -1,5 +0,0 @@
-file.o: file.c file.h
-test.o: test.c file.h log.h util.h
-time.o: time.c time.h
-util.o: util.c util.h
-xcb.o: xcb.c xcb.h JBDim.h log.h util.h
--- libjb/time.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2017, Jeffrey E. Bedard
-#ifndef LIBJB_TIME_H
-#define LIBJB_TIME_H
-#include <stdint.h>
-void jb_sleep(uint32_t miliseconds);
-#endif//!LIBJB_TIME_H
