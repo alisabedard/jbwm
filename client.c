14df5d49b350930dad8e052cbb5b459c33f2633c
Jeffrey E. Bedard <jefbed@gmail.com>
2020-01-01 18:21:25 -0500

    Changed copyright end year to 2020.

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 // jbwm - Minimalist Window Manager for X
-// Copyright 2008-2017, Jeffrey E. Bedard <jefbed@gmail.com>
+// Copyright 2008-2020, Jeffrey E. Bedard <jefbed@gmail.com>
 // Copyright 1999-2015, Ciaran Anscomb <evilwm@6809.org.uk>
 // See README for license and other details.
 #include "client.h"
