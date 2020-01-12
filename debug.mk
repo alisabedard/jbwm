b799f131f12627e2f1dcbc25d8d18df151f4e21a
Jeffrey E. Bedard <jefbed@gmail.com>
2020-01-12 01:38:01 -0500

    Removed redundancy with debug_gcc.mk.

--- debug.mk
+++ debug.mk
@@ -1,9 +1,5 @@
 # Debug
 jbwm_cflags=-O0 -ggdb -DDEBUG
-# Profile
-jbwm_cflags+=-pg
-# Enable all warnings
-jbwm_cflags+=-W -Wall -Wextra -Werror
 # Select compiler
 #include debug_clang.mk
 include debug_gcc.mk
