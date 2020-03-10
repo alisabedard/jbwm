ceb6b4ce4a938504423dc11dc718684f5b69b34c
Jeffrey E. Bedard <jefbed@gmail.com>
2020-03-10 01:29:54 -0400

    Removed -e option to echo.

--- mkdep.sh
+++ mkdep.sh
@@ -2,11 +2,11 @@
 OUT=depend.mk
 for SRC in *.c; do
     LINE="`basename $SRC .c`.o: $SRC"
-    for DEP in `grep '#include "' $SRC \
-        | cut -d' ' -f2 \
-        | tr -d \" `; do
-	LINE="$LINE $DEP"
+    for DEP in `fgrep '#include "' $SRC \
+            | cut -d' ' -f2 \
+            | tr -d \" `; do
+        LINE="$LINE $DEP"
     done
     LINES="$LINES$LINE\n"
 done
-echo -e $LINES > $OUT
+echo $LINES > $OUT
