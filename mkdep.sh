2dd794794c066c69258aa42f59d08ce0114f7fdc
Jeffrey E. Bedard <jefbed@gmail.com>
2020-03-13 00:44:37 -0400

    Changed to run with busybox ash.

--- mkdep.sh
+++ mkdep.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 OUT=depend.mk
+rm -f $OUT
 for SRC in *.c; do
     LINE="`basename $SRC .c`.o: $SRC"
     for DEP in `fgrep '#include "' $SRC \
@@ -7,6 +8,6 @@ for SRC in *.c; do
             | tr -d \" `; do
         LINE="$LINE $DEP"
     done
-    LINES="$LINES$LINE\n"
+    echo $LINE >> $OUT
 done
-echo $LINES > $OUT
+
