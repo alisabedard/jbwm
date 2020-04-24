6f8e01695411511ee13b8f795ec4f69bd59c2df1
Jeffrey E. Bedard <jefbed@gmail.com>
2020-04-24 06:04:34 -0400

    Reset target if !*current_client.

--- key_event.c
+++ key_event.c
@@ -228,6 +228,8 @@ void jbwm_handle_key_event(struct JBWMScreen * s, struct JBWMClient * target,
   default:
     if (!target)
       target = *current_client;
+    if (!target)
+      target = *head_client;
     if (target) {
       handle_client_key_event(head_client, current_client, flags & MOD, key);
 #ifdef DEBUG
