9fce6968886f1531635c9faeda339134baa1dae1
Jeffrey E. Bedard <jefbed@gmail.com>
2020-06-27 16:33:39 -0400

    Disabled larger than errors.

--- debug_clang.mk
+++ debug_clang.mk
@@ -2,8 +2,8 @@
 CC=clang
 jbwm_cflags+=-W -Wall -Wextra -Werror
 jbwm_cflags+=-Wuninitialized -Wstrict-overflow=5
-jbwm_cflags+=-Wlarger-than=512
-jbwm_cflags+=-Wframe-larger-than=512
+#jbwm_cflags+=-Wlarger-than=512
+#jbwm_cflags+=-Wframe-larger-than=512
 jbwm_cflags+=-Wcast-qual -Wcast-align
 jbwm_cflags+=-Weverything
 jbwm_cflags+=-Wno-conversion
--- debug_gcc.mk
+++ debug_gcc.mk
@@ -9,8 +9,8 @@ jbwm_cflags+=-Wsuggest-attribute=pure
 jbwm_cflags+=-Wsuggest-attribute=const
 jbwm_cflags+=-Wsuggest-attribute=noreturn
 jbwm_cflags+=-Wsuggest-attribute=format
-jbwm_cflags+=-Wlarger-than=512
-jbwm_cflags+=-Wframe-larger-than=512
+#jbwm_cflags+=-Wlarger-than=512
+#jbwm_cflags+=-Wframe-larger-than=512
 jbwm_cflags+=-Wcast-qual -Wcast-align
 jbwm_cflags+=-Wjump-misses-init
 jbwm_cflags+=-Wlogical-op
